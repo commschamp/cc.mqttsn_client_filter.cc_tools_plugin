65608e020e39e660c0b75191f393b68703516081
Alex Robenko <arobenko@gmail.com>
2024-08-27 09:03:48 +1000

    Require v2.0.1 of the cc.mqttsn.libs.

--- src/MqttsnClientFilter.h
+++ src/MqttsnClientFilter.h
@@ -32,8 +32,8 @@
 #include <memory>
 #include <string>
 
-static_assert(CC_MQTTSN_CLIENT_MAKE_VERSION(2, 0, 0) <= CC_MQTTSN_CLIENT_VERSION, "The version of the cc_mqttsn_client library is too old");
-static_assert(CC_TOOLS_QT_MAKE_VERSION(5, 3, 0) <= CC_TOOLS_QT_VERSION, "The version of the cc_tools_qt library is too old");
+static_assert(CC_MQTTSN_CLIENT_MAKE_VERSION(2, 0, 1) <= CC_MQTTSN_CLIENT_VERSION, "The version of the cc_mqttsn_client library is too old");
+static_assert(CC_TOOLS_QT_MAKE_VERSION(5, 3, 1) <= CC_TOOLS_QT_VERSION, "The version of the cc_tools_qt library is too old");
 
 namespace cc_plugin_mqttsn_client_filter
 {
